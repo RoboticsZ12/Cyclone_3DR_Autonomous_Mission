7fa985bbd458cf6e52005115c373fc586e976d65
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-06-22 12:44:46 -0500

    new js file

---
+++ Cyclone_3DR/src/TEST.js
@@ -0,0 +1,40 @@
+//******************************//
+//**POSSIBLE EXAMPLE FUNCTIONS**//
+//******************************//
+
+// Define the initila x,y,z reference point (Fiducial)
+function InitialPt(InitialX, InitialY, InitialZ)
+{
+
+}
+
+// Defining lattitude and longitude of map
+function LongLat(Long, Lat)
+{
+
+}
+
+// Define number of waypoints wanted
+function NumWaypoints(NumPts)
+{
+
+}
+
+// Define distance from one waypoint to the next (EX: InitialX+1)
+// Will need to know what units Cyclone use for "+1"
+function PointIncrementation(IncrementX, IncrementY, IncrementZ)
+{
+
+}
+
+// Adding point to position after incrementation
+function AddWaypoint(AddPt)
+{
+
+}
+
+// Duration of wait time for spot at each point
+function Spotwait(Duration)
+{
+    
+}
