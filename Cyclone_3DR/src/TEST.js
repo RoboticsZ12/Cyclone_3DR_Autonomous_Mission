5f30d8b0dc307aa97bd9e686a3a9209759e054d5
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-06-22 13:39:04 -0500

    update user input in code

--- Cyclone_3DR/src/TEST.js
+++ Cyclone_3DR/src/TEST.js
@@ -38,3 +38,30 @@ function Spotwait(Duration)
 {
     
 }
+
+// MAIN
+
+// Ask user initial x, y, z (fiducial)
+let InitialX = parseFloat(prompt("Select starting x coordinate (Type with decimal): "));
+let InitialY = parseFloat(prompt("Select starting y coordinate (Type with decimal): "));
+let InitialZ = parseFloat(prompt("Select starting z coordinate (Type with decimal): "));
+InitialPt(); // Call initial point function 
+
+
+let Long = parseFloat(prompt("Enter the Longitude of the station (Type with decimal): "));
+let Lat = parseFloat(prompt("Enter the Latitude of the station (Type with decimal): "));
+LongLat(); // Call Long/Lat function
+
+let NumPts = parse(prompt("Enter number of waypoints for this mission: "));
+NumWaypoints(); // Call Number of Waypoint function 
+
+let IncrementX = parse(prompt("Enter incrementation in X: "));
+let IncrementY = parse(prompt("Enter incrementation in Y: "));
+let IncrementZ = parse(prompt("Enter incrementation in Z: "));
+PointIncrementation(); // Call point of incrementation function
+
+let AddPt = parseFloat(prompt("Select next waypoint mission"));
+AddWaypoint(); // Call next waypoint function 
+
+let Spotwait = parse(prompt("How long should spot wait between each point? (Seconds): "));
+Spotwait(); // call spot robot wait function
