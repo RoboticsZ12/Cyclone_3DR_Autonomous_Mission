d965d1f7c9bbb6f4ed09c0fa0d4b0740f99de2ed
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-06-26 18:39:01 -0500

    TEST2 PUSH

--- Cyclone_3DR/src/TEST.js
+++ Cyclone_3DR/src/TEST.js
@@ -2,196 +2,216 @@
 //**POSSIBLE EXAMPLE FUNCTIONS**//
 //******************************//
 
-// Global variables
-var XYZ;
-var dialogInitialXYZ;
-
-
-
-// Define the initila x,y,z reference point (Fiducial)
-function InitialPt(InitialX, InitialY, InitialZ) {
-    // Create the dialog for Initial X, Y, Z Values
-    var XYZ = SDialog.New("Initial X,Y,Z Values");
-
-    // Add input fields for X, Y, Z with initial values
-    XYZ.AddLength({id: 'X', name: "Initial X Pos", value: InitialX, saveValue: true, readOnly: false});
-    XYZ.AddLength({id: 'Y', name: "Initial Y Pos", value: InitialY, saveValue: true, readOnly: false});
-    XYZ.AddLength({id: 'Z', name: "Initial Z Pos", value: InitialZ, saveValue: true, readOnly: false});
-
-    // Run the dialog to get user inputs
-    var dialogInitialXYZ = XYZ.Run();
-
-    // Retrieve the initial X, Y, Z values entered by the user
-    var initialXValue = dialogInitialXYZ.X;
-    var initialYValue = dialogInitialXYZ.Y;
-    var initialZValue = dialogInitialXYZ.Z;
-
-    // Validate the initial X value 
-    if (initialXValue >= 90 || initialYValue < 0) 
-    {
-        throw new Error("INVALID");
-    }
-
-    // Validate the initial Y value 
-    if (initialYValue >= 90 || initialYValue < 0) 
-    {
-        throw new Error("INVALID");
-    }
-
-    // Validate the initial Z value 
-    if (initialZValue < 0) 
-    {
-        throw new Error("INVALID");
-    }
-
-    // Return the initial X, Y, Z values
-    return {initialXValue, initialYValue, initialZValue}; // Return an object containing all values
-}
-
-// Defining lattitude and longitude of map
-function LongLat(Long, Lat)
-{
-    var LongLatLength = SDialog.New("Latitude/Longitude Assingment");      
-
-    // Input Field Here
-    LongLatLength.AddLength({id: 'Lat', name: "Latitidue Value", value: Lat, saveValue: true, readOnly: false});
-    LongLatLength.AddLength({id: 'Long', name: "Longitude Value", value: Long, saveValue: true, readOnly: false});
-
-    // User input
-    var dialogLongLat = LongLatLength.Run();
-
-    // Lat/Long value retrieval
-    var Latitudinal = dialogLongLat.Lat;
-    var Longitudinal = dialogLongLat.Long;
-
-    // Validate Lat value of substation
-    if (Latitudinal >= 90) 
-    {
-        throw new Error("Latitude value to large");
-    }
-    else if(Latitudinal < 0)
-    {
-        throw new Error("Latitude value to small");
-    }
-
-    // Validate Long value of substation
-    if(Longitudinal >= 90)
-    {
-        throw new Error("Longitude value to large");
-    }
-    else if(Longitudinal < 0)
-    {
-        throw new Error("Longtitude value to small");
-    }
-}
-
-// Define number of waypoints wanted
-function NumWaypoints(NumPts)
-{
-    var WantedWaypoints = SDialog.New("Number Waypoint Mission Points");    
-    WantedWaypoints.AddLength({id: 'Waypoints', name: "# Waypoints", value: NumPts, saveValue: true, readOnly: false});
-    
-    // User input
-    var dialogWaypoints = WantedWaypoints.Run();
-
-    // Retrieve the initial X, Y, Z values entered by the user
-    var WayMission = dialogWaypoints.Waypoints;
-
-    if(WayMission <= 0 || WayMission >= 5000)
-    {
-        throw new Error("INVALID AMOUNT OF POINTS");
-    }
-}
-
-// Define distance from one waypoint to the next 
-function PointIncrementation(IncrementX, IncrementY, IncrementZ)
-{
-    var IncrementationXYZ = SDialog.New("Increment X,Y,Z");
-
-    // Setting up user input window
-    IncrementationXYZ.AddLength({id: 'X_length', name: "X Increment", value: IncrementX, saveValue: true, readOnly: false});
-    IncrementationXYZ.AddLength({id: 'Y_length', name: "Y Increment", value: IncrementY, saveValue: true, readOnly: false});
-    IncrementationXYZ.AddLength({id: 'Z_length', name: "Z Increment", value: IncrementZ, saveValue: true, readOnly: false});
-
-    // User input recorded
-    var dialogNewXYZ = IncrementationXYZ.Run();
-
-    // Recalling user inputs
-    var NewXrecall = dialogNewXYZ.X_length;
-    var NewYrecall = dialogNewXYZ.Y_length;
-    var NewZrecall = dialogNewXYZ.Z_length;
-
-    // if(NewXrecall > Longitudinal || NewXrecall > Latitudinal)
-    // {
-    //     throw new Error("Please stay within Long and Lat bounds");
-    // }
-    // else if(NewYrecall > Longitudinal || NewYrecall > Latitudinal)
-    // {
-    //     throw new Error("Please stay within Long and Lat bounds");
-    // }
-    // else if(NewZrecall > Longitudinal || NewZrecall > Latitudinal)
-    // {
-    //     throw new Error("Please stay within Long and Lat bounds");
-    // }
-}
-
-// Adding point to position after incrementation
-function AddWaypoint(AddPt)
-{
-    var CurrentPoint = SPoint.New(InitialX, InitialY, InitialZ);
-    if(CurrentPoint < Lat && CurrentPoint <= Long)
-    {
-        CurrentPoint = CurrentPoint.Add(SPoint.New(NumPts,0,0));
-    }
-    else if(CurrentPoint == Lat && CurrentPoint == Long)
-    {
-        CurrentPoint = CurrentPoint.Add(SPoint.New(0,NumPts,0));
-    }
-    else
-    {
-        ErrorMessage("Outside of Long and Lat boundry!");
-    }
-
-}
-
-// Duration of wait time for spot at each point
-function SpotWait(Duration)
-{
-    var SpotStop = SDialog.New("Spot Stop Time");
-
-    // Setting up user input
-    SpotStop.AddLength({id: 'Stop_Time', name: "Time At Stop", value: Duration, saveValue: true, readOnly: false});
-   
-    // User input recorded
-    var StopResult = SpotStop.Run();
-
-    // Recalling user input
-    var SpotStepValue = StopResult.Stop_Time;
-
-    // Checking input value valid
-    if(SpotStepValue <= 0 || SpotStepValue > 10)
-    {
-        throw new Error("Please choose a resonable stopping value");
-    }
-}
-
 // Error Message Function
 function ErrorMessage(iMessage) 
 {
     SDialog.Message(iMessage,SDialog.EMessageSeverity.Error,"Error");
     throw new Error(iMessage);
 }
-// var imessage = "no";
-// ErrorMessage(imessage);
 
+// Open Point Cloud
+function openMyproject(iName) 
+{
+    var isOpen = OpenDoc(iName, true, true).ErrorCode; // 3DR document is cleared and the defined 3DR project is opened 
 
+    if (isOpen == 1) {
+        ErrorMessage("An error occurred. Impossible to open the 3DR file."); // print an error message if no success
+    }
+    else 
+    {
+        print("Your project " + iName + " has been opened."); // throw a message to inform the opening of the file
+    }
+}
 // MAIN
-InitialPt(1,1,1);
+// 4. Add waypoints
+	workflowStep++;
+	print("Step" + workflowStep + ": Add waypoints");
+	errorMsg = "";
+	count = 1 + myMission.WaypointsTbl.length;
 
-LongLat(0,0);
+	if(ValidateAStep(
+		   "Add Waypoints",
+		   "Click to define Waypoint? (" + count + ") (Press ESC to stop)",
+		   "Yes=Continue / No=go to 'return' waypoints definition"))
+	{
+		var allOK = true;
 
-PointIncrementation(1,1,1);
+		do
+		{
+			var pointRes = SPoint.FromClick();
+			if(pointRes.ErrorCode == 0)
+			{ // point clicked by user
+				var newPoint = pointRes.Point;
 
-NumWaypoints(1);
+				//waypoint projection
+				newPoint = myMission.RefPlane.Proj3D(newPoint).Point;
 
-SpotWait(1);
+				//waypoint verification
+				var verified = IsWaypointAllowed(newPoint, [myMission.GoZone], myMission.NoGoZonesTbl);
+				if(verified)
+				{
+					newPoint.SetName(myMission.MissionName + "_" + count);
+
+					//Waypoint creation
+					var newWayPoint1 = SWaypoint.CreateWayPoint(myMission, count, newPoint, "1", "None");
+
+					myMission.WaypointsTbl.push(newWayPoint1);
+					myMission.UpdateDummyPath();
+
+					count++;
+				}
+				else
+				{
+					ErrorMessage("The point is not valid according to GO-NO GO Zones", false);
+				}
+			}
+			else // Escape or Enter -> stopping
+			{
+				allOK = false;
+			}
+        }
+        while(allOK);
+	}
+
+
+
+// Open an existing 3DR file
+    var myfileName = GetOpenFileName("Select the file to open", "3DR files (*.3dr)", "C://"); // Define the path and the name of your file
+    if (myfileName.length == 0)
+    {
+        ErrorMessage("Operation cancelcyed");
+    }
+
+    openMyproject(myfileName);
+
+// Create the dialog for Initial X, Y, Z Values
+    var XYZ = SDialog.New("Initial X,Y,Z Values");
+
+    // Add input fields for X, Y, Z with initial values
+    XYZ.AddLength({id: 'X', name: "Initial X Pos", value: 0, saveValue: true, readOnly: false});
+    XYZ.AddLength({id: 'Y', name: "Initial Y Pos", value: 0, saveValue: true, readOnly: false});
+    XYZ.AddLength({id: 'Z', name: "Initial Z Pos", value: 0, saveValue: true, readOnly: false});
+
+    // Run the dialog to get user inputs
+    var dialogInitialXYZ = XYZ.Run();
+
+    // Check if "Cancel" button is pressed
+    if (dialogInitialXYZ.ErrorCode == 1) 
+    {
+        var imessage = "User Has Terminated Sequence";
+        ErrorMessage(imessage);
+    }
+    else
+    {
+        // Retrieve the initial X, Y, Z values entered by the user
+        var initialXValue = dialogInitialXYZ.X;
+        var initialYValue = dialogInitialXYZ.Y;
+        var initialZValue = dialogInitialXYZ.Z;
+
+        // Validate the initial X value 
+        if (initialXValue >= 90 || initialYValue < 0) 
+        {
+            var imessage = "Invalid X or Y Value";
+            ErrorMessage(imessage);
+        }
+
+        // Validate the initial Y value 
+        if (initialYValue >= 90 || initialYValue < 0) 
+        {
+            var imessage = "Invalid X or Y Value";
+            ErrorMessage(imessage);
+        }
+
+        // Validate the initial Z value 
+        if (initialZValue < 0) 
+        {
+            var imessage = "Invalid Z Value";
+            ErrorMessage(imessage);
+        }
+    }   
+
+// Number of Waypoints
+    var WantedWaypoints = SDialog.New("Number Waypoint Mission Points");
+    
+    // Run the dialog to get user inputs
+    WantedWaypoints.AddLength({id: 'Waypoints', name: "# Waypoints", value: 0, saveValue: true, readOnly: false});
+        
+    // User input
+    var dialogWaypoints = WantedWaypoints.Run();
+
+    // Check if "Cancel" button is pressed
+    if (dialogWaypoints.ErrorCode == 1) 
+    {
+        var imessage = "User Has Terminated Sequence";
+        ErrorMessage(imessage);
+    }
+    else
+    {
+        // Retrieve the initial X, Y, Z values entered by the user
+        var WayMission = dialogWaypoints.Waypoints;
+        
+        if(WayMission <= 0 || WayMission >= 5000)
+        {
+            var imessage = "Invalid Waypoint Value";
+            ErrorMessage(imessage);
+        }
+    }
+
+// Incrementation of new XYZ
+    var IncrementationXYZ = SDialog.New("Increment X,Y,Z");
+
+    // Setting up user input window
+    IncrementationXYZ.AddLength({id: 'X_length', name: "X Increment", value: 0, saveValue: true, readOnly: false});
+    IncrementationXYZ.AddLength({id: 'Y_length', name: "Y Increment", value: 0, saveValue: true, readOnly: false});
+    IncrementationXYZ.AddLength({id: 'Z_length', name: "Z Increment", value: 0, saveValue: true, readOnly: false});
+    
+    // User input recorded
+    var dialogNewXYZ = IncrementationXYZ.Run();
+
+    // Check if "Cancel" button is pressed
+    if (dialogNewXYZ.ErrorCode == 1) 
+    {
+        var imessage = "User Has Terminated Sequence";
+        ErrorMessage(imessage);
+    }
+    else
+    {
+        // Recalling user inputs
+        var NewXrecall = dialogNewXYZ.X_length;
+        var NewYrecall = dialogNewXYZ.Y_length;
+        var NewZrecall = dialogNewXYZ.Z_length;
+    }
+
+// Duration of wait time for spot at each point
+    var SpotStop = SDialog.New("Spot Stop Time");
+
+    // Setting up user input
+    SpotStop.AddLength({id: 'Stop_Time', name: "Time At Stop", value: 0, saveValue: true, readOnly: false});
+
+    // User input recorded
+    var StopResult = SpotStop.Run();
+
+    // Check if "Cancel" button is pressed
+    if (StopResult.ErrorCode == 1) 
+    {
+        var imessage = "User Has Terminated Sequence";
+        ErrorMessage(imessage);
+    }
+    else
+    {
+        // Recalling user input
+        var SpotStepValue = StopResult.Stop_Time;
+    
+        // Checking input value valid
+        if(SpotStepValue <= 0 || SpotStepValue > 10)
+        {
+            var imessage = "Invalid Spot Wait Time";
+            ErrorMessage(imessage);
+        }  
+    }
+    
+    // Printing all Variables
+    print("Chosen Reference Pts: " + initialXValue + " X, " + initialYValue + " Y, " + initialZValue + " Z");
+    print("Chosen # Waypoint: " + WayMission);
+    print("XYZ Incrementation Values: " + NewXrecall + " X, " + NewYrecall + " Y, " + NewZrecall + " Z");
+    print("Set Wait Time: " + SpotStepValue);
